b8a3dce2de971e7329cc9561e7219a201510e22a
Mr.Salih <salihbaz07@gmail.com>
2023-03-30 00:50:31 +0300

    add new code rstr_capitalizer

--- Level 3/rstr_capitalizer/rstr_capitalizer.c
+++ Level 3/rstr_capitalizer/rstr_capitalizer.c
@@ -1,35 +1,75 @@
 #include <unistd.h>
 
-void    rstr_capitalizer(char *str)
+void    ft_capi(char *str)
 {
     int i = 0;
 
-    while (str[i])
+    while(str[i])
     {
-        if (str[i] >= 'A' && str[i] <= 'Z')
+        if(str[i] >= 'A' && str[i] <= 'Z' && str[i + 1] != ' ' && str[i + 1])
             str[i] += 32;
-        if ((str[i] >= 'a' && str[i] <= 'z') && (str[i + 1] == ' ' \
-                    || str[i + 1] == '\t' || str[i + 1] == '\0'))
-            str[i] -= 32;
-        write(1, &str[i++], 1);
+        i++;
     }
+    i = 0;
+    while(str[i])
+    {
+        if((str[i + 1] == ' ' || str[i + 1] == '\0') && (str[i] >= 'a' && str[i] <= 'z'))
+            str[i] -= 32;
+        i++;
+    }
+    i = 0;
+    while(str[i])
+        write(1, &str[i++], 1);
 }
 
-int main(int argc, char **argv)
+int main(int ac, char **av)
 {
-    int i;
+    int i = 1;
 
-    if (argc == 1)
-        write(1, "\n", 1);
-    else
+    if(ac > 1)
     {
-        i = 1;
-        while (i < argc)
+        while(av[i])
         {
-            rstr_capitalizer(argv[i]);
+            ft_capi(av[i++]);
             write(1, "\n", 1);
-            i += 1;
         }
     }
-    return (0);
+    else
+        write(1, "\n", 1);
 }
+
+// #include <unistd.h>
+
+// void    rstr_capitalizer(char *str)
+// {
+//     int i = 0;
+
+//     while (str[i])
+//     {
+//         if (str[i] >= 'A' && str[i] <= 'Z')
+//             str[i] += 32;
+//         if ((str[i] >= 'a' && str[i] <= 'z') && (str[i + 1] == ' ' \
+//                     || str[i + 1] == '\t' || str[i + 1] == '\0'))
+//             str[i] -= 32;
+//         write(1, &str[i++], 1);
+//     }
+// }
+
+// int main(int argc, char **argv)
+// {
+//     int i;
+
+//     if (argc == 1)
+//         write(1, "\n", 1);
+//     else
+//     {
+//         i = 1;
+//         while (i < argc)
+//         {
+//             rstr_capitalizer(argv[i]);
+//             write(1, "\n", 1);
+//             i += 1;
+//         }
+//     }
+//     return (0);
+// }
