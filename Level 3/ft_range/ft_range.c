c75bb501ff9cf9771e3f2a8daf970e7ad30a4d66
salihprnz07 <salihbaz07@gmail.com>
2023-03-29 18:07:28 +0300

    Update ft_range.c

--- Level 3/ft_range/ft_range.c
+++ Level 3/ft_range/ft_range.c
@@ -1,23 +1,5 @@
 #include <stdlib.h>
 
-int	*ft_range(int start, int end)
-{
-	int	*res;
-	int	size;
-	int	i;
-
-	size = (end - start) + 1;
-	i = 0;
-	res = (int *) malloc(size * sizeof(int));
-	if (!res)
-		return (NULL);
-	while (i++ < size)
-		res[i] = start + i;
-	return (res);
-}
-/* 
-#include <stdlib.h>
-
 int		ft_abs(int x)
 {
 	if (x < 0)
@@ -48,4 +30,34 @@ int		*ft_range(int start, int end)
 	tab[i] = start;
 	return (tab);
 }
- */
+
+/*#include <stdlib.h>
+
+int	*ft_range(int start, int end)
+{
+	int	*res;
+	int	size;
+	int	i;
+
+	size = (end - start) + 1;
+	i = 0;
+	res = (int *) malloc(size * sizeof(int));
+	if (!res)
+		return (NULL);
+	while (i++ < size)
+		res[i] = start + i;
+	return (res);
+}
+*/
+
+// #include <stdio.h>
+// int main()
+// {
+// 	int i = 0;
+// 	int *arr = ft_rrange(1,-3);
+// 	while(i < 4)
+// 	{
+// 		printf("%d",arr[i]);
+// 		i++;
+// 	}
+// }
