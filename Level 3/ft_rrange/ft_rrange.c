af99a1c5fc123a94c011376d3b22394794a59563
salihprnz07 <salihbaz07@gmail.com>
2023-03-31 23:28:18 +0300

    Update ft_rrange.c

--- Level 3/ft_rrange/ft_rrange.c
+++ Level 3/ft_rrange/ft_rrange.c
@@ -1,70 +1,30 @@
 #include <stdlib.h>
 
-int     *ft_range(int start, int end)
+int     *ft_rrange(int start, int end)
 {
-	int size = 0;
-	int *arr;
-	int start2 = start;
-	
-	if(start > end)
-		size = start - end;
-	else
-		size = end - start;
-	arr = (int*)malloc(sizeof(int)*(size + 1));
-	int i = 0;
-	while(size >= 0)
-	{
-		arr[i] = end;
-		if(start2 > end)
-			end++;
-		else
-			end--;
-		size--;
-		i++;
-	}
-	return arr;
+    int i = 0;
+    int size;
+    size = (start <= end) ? (end - start + 1) : (start - end + 1);
+
+    int *res = (int *)malloc(sizeof(int) * size);
+    if(!res)
+        return NULL;
+
+    while(i < size)
+    {
+        res[i] = end;
+        end += (start <= end) ? -1 : 1;
+        i++;
+    }
+    return res;
 }
-
-/* #include <stdlib.h>
-
-int		ft_abs(int x)
-{
-	if (x < 0)
-		return (-x);
-	return (x);
-}
-
-int		*ft_rrange(int start, int end)
-{
-	int i;
-	int *tab;
-
-	i = 0;
-	tab = (int*)malloc(sizeof(int) * ft_abs(end - start) + 1);
-	while (end > start)
-	{
-		tab[i] = end;
-		end--;
-		i++;
-	}
-	tab[i] = end;
-	while (end < start)
-	{
-		tab[i] = end;
-		end++;
-		i++;
-	}
-	tab[i] = end;
-	return (tab);
-}
- */
 
 // #include <stdio.h>
 // int main()
 // {
 // 	int i = 0;
-// 	int *arr = ft_range(0,-3);
-// 	while(i < 4)
+// 	int *arr = ft_rrange(1 ,3);
+// 	while(i < 3)
 // 	{
 // 		printf("%d",arr[i]);
 // 		i++;
