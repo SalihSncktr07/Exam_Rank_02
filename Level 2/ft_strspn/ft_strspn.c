f5c15cae9f73e26a40553f3060d67453348c576b
Mr.Salih <salihbaz07@gmail.com>
2023-03-31 11:46:31 +0300

    Update ft_strspn.c

--- Level 2/ft_strspn/ft_strspn.c
+++ Level 2/ft_strspn/ft_strspn.c
@@ -1,25 +1,24 @@
-#include <string.h>
-
-char	*ft_strchr(const char *s, int c)
-{
-	while (*s != '\0')
-	{
-		if (*s == c)
-			return ((char *)s);
-		++s;
-	}
-	return (0);
-}
+#include <stddef.h>
 
 size_t	ft_strspn(const char *s, const char *accept)
 {
-	size_t i = 0;
-
-	while (s[i] != '\0')
-	{
-		if (ft_strchr(accept, s[i]) == 0)
-			break;
-		++i;
-	}
-	return (i);
+    size_t i = 0;
+    while (s[i])
+    {
+        size_t j = 0;
+        while (accept[j])
+        {
+            if (s[i] == accept[j])
+            {
+                break;
+            }
+            j++;
+        }
+        if (!accept[j])
+        {
+            return i;
+        }
+        i++;
+    }
+    return i;
 }
